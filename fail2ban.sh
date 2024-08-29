36a03fe43c61528cd53977f6200dee5d04f9c73b
xz2dai <40030489+xz2dai@users.noreply.github.com>
2024-08-29 11:57:27 +0800

    Update fail2ban.sh

--- fail2ban.sh
+++ fail2ban.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # 安装 fail2ban
-sudo apt update
 sudo apt install fail2ban -y
 
 
@@ -18,7 +17,7 @@ maxretry = 3
 EOF'
 
 # 重启 fail2ban 服务
-sudo systemctl restart fail2ban.service
+sudo systemctl restart fail2ban
 
 # 查看 fail2ban 状态
-sudo fail2ban-client status sshd
+sudo fail2ban status sshd
